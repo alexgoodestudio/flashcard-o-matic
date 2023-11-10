937917b1483e52d41e43de11f9d2ef4fc9a3531f
Alex Goode <alexgoodestudio@gmail.com>
2023-11-10 18:35:01 -0500

    fixed spacing

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,15 +1,16 @@
 import React from "react";
 import image1 from "../IMAGES/8.png"
 
+
 function Header() {
   return (
     <>
 
-      <img src={image1} alt="banner" className="w-100"/>
-      <div className="container text-white">
+      <img src={image1} alt="banner" className="w-100 mb-5 mq"/>
+      {/* <div className="container text-white">
         <h1 className="display-4">Flashcard-o-matic</h1>
         <p className="lead">Discover The Flashcard Difference.</p>
-      </div>
+      </div> */}
     
     </>
   );
